6dea143737f6a3e765babb15d49e47449c46a20b
Atılhan Emre Dursunoğlu <atilhandursunoglu@outlook.com>
2021-08-03 22:47:31 +0300

    [REFACTOR] nms-simd - Fix numerous bugs. - Add an entrypoint function for nms-simd. - Add namespace. - Change absl::random to pcg.

---
+++ nms-simd/nms-simd.cpp
@@ -0,0 +1,185 @@
+#include <immintrin.h>
+#include <algorithm>
+
+#include "nms-simd.h"
+
+__forceinline __m256 compareRectangles(size_t readIdx,
+									   __m256i passX1_8,
+									   __m256i passX2_8,
+									   __m256i passY1_8,
+									   __m256i passY2_8,
+									   __m256i passArea_8,
+									   __m256i x1_8,
+									   __m256i x2_8,
+									   __m256i y1_8,
+									   __m256i y2_8,
+									   __m256 threshold_8)
+{
+	const auto zero_8 = _mm256_set1_epi32(0);
+	const auto one_8 = _mm256_set1_epi32(1);
+
+	auto interX1_8 = _mm256_max_epi32(x1_8, passX1_8);
+	auto interX2_8 = _mm256_min_epi32(x2_8, passX2_8);
+	auto interY1_8 = _mm256_max_epi32(y1_8, passY1_8);
+	auto interY2_8 = _mm256_min_epi32(y2_8, passY2_8);
+
+	// (interX2 - interX1)
+	auto interXDiff_8 = _mm256_sub_epi32(interX2_8, interX1_8);
+
+	// (interY2 - interY1)
+	auto interYDiff_8 = _mm256_sub_epi32(interY2_8, interY1_8);
+
+	auto bboxWidth_8 = _mm256_max_epi32(zero_8, interXDiff_8);
+	auto bboxHeight_8 = _mm256_max_epi32(zero_8, interYDiff_8);
+
+	auto interArea_8 = _mm256_mullo_epi32(bboxWidth_8, bboxHeight_8);
+
+	auto width_8 = _mm256_sub_epi32(x2_8, x1_8);
+	auto height_8 = _mm256_sub_epi32(y2_8, y1_8);
+	auto area_8 = _mm256_mullo_epi32(width_8, height_8);
+
+	// area + passArea
+	auto areaSum_8 = _mm256_add_epi32(area_8, passArea_8);
+	// (area + passArea) - interArea
+	areaSum_8 = _mm256_sub_epi32(areaSum_8, interArea_8);
+
+	auto areaSum_8f = _mm256_cvtepi32_ps(areaSum_8);
+	auto interArea_8f = _mm256_cvtepi32_ps(interArea_8);
+
+	auto overlap_8 = _mm256_div_ps(interArea_8f, areaSum_8f);
+
+	return _mm256_cmp_ps(threshold_8, overlap_8, 13);
+}
+
+void NMS_SIMD::nmsSimd1(const Rectangles& rects, float threshold)
+{
+	size_t passIdx = 0;
+	size_t passCount = 1;
+
+	auto threshold_8 = _mm256_set1_ps(threshold);
+
+	for(size_t pass = 0; pass < passCount; pass++)
+	{
+		bool isFound = false;
+
+		auto passX1_8 = _mm256_set1_epi32(rects.x1[passIdx]);
+		auto passX2_8 = _mm256_set1_epi32(rects.x2[passIdx]);
+		auto passY1_8 = _mm256_set1_epi32(rects.y1[passIdx]);
+		auto passY2_8 = _mm256_set1_epi32(rects.y2[passIdx]);
+
+		auto passWidth = rects.x2[passIdx] - rects.x1[passIdx];
+		auto passHeight = rects.y2[passIdx] - rects.y1[passIdx];
+		auto passArea_8 = _mm256_set1_epi32(passWidth * passHeight);
+
+		size_t passIdxTemp = passIdx;
+
+		for(size_t readIdx = passIdxTemp + 1; readIdx < rects.count; readIdx += 8)
+		{
+			int control = false;
+			auto validness_8 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(rects.validness + readIdx));
+			control = _mm256_movemask_epi8(validness_8);
+			const size_t remainingCount = 8;
+
+			if(!control)
+			{
+				continue;
+			}
+
+			auto x1_8 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(rects.x1 + readIdx));
+			auto x2_8 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(rects.x2 + readIdx));
+			auto y1_8 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(rects.y1 + readIdx));
+			auto y2_8 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(rects.y2 + readIdx));
+
+			auto isValid_8 = compareRectangles(readIdx,
+											   passX1_8,
+											   passX2_8,
+											   passY1_8,
+											   passY2_8,
+											   passArea_8,
+											   x1_8,
+											   x2_8,
+											   y1_8,
+											   y2_8,
+											   threshold_8);
+
+			auto isValid_8i = _mm256_cvtps_epi32(isValid_8);
+
+			isValid_8i = _mm256_and_si256(isValid_8i, validness_8);
+			auto isValid = _mm256_movemask_ps(_mm256_castsi256_ps(isValid_8i));
+			_mm256_storeu_si256(reinterpret_cast<__m256i*>(rects.validness + readIdx), isValid_8i);
+
+			if(!isFound && isValid)
+			{
+				for(size_t ele = 0; ele < remainingCount; ele++)
+				{
+					if(rects.validness[readIdx + ele])
+					{
+						isFound = true;
+						passIdx = readIdx + ele;
+						passCount++;
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+std::vector<size_t> NMS_SIMD::runNmsSimd1(const std::vector<Rect>& rects, const std::vector<float>& scores, float scoreThreshold, float nmsThreshold)
+{
+	std::vector<size_t> passRectIndices;
+	for(size_t i = 0; i < scores.size(); i++)
+	{
+		if(scores[i] >= scoreThreshold)
+		{
+			passRectIndices.push_back(i);
+		}
+	}
+
+	std::sort(passRectIndices.begin(), passRectIndices.end(), [&](size_t a, size_t b) {
+		return scores[a] > scores[b];
+			  });
+
+	Rectangles simdRects;
+	simdRects.count = passRectIndices.size();
+	simdRects.simdCount = (simdRects.count + 7) / 8 * 8 + 7;
+	uint32_t* buffer = new uint32_t[5 * simdRects.simdCount];
+	simdRects.x1 = buffer;
+	simdRects.x2 = buffer + simdRects.simdCount;
+	simdRects.y1 = buffer + simdRects.simdCount * 2;
+	simdRects.y2 = buffer + simdRects.simdCount * 3;
+	simdRects.validness = buffer + simdRects.simdCount * 4;
+
+	for(size_t i = 0; i < simdRects.count; i++)
+	{
+		simdRects.validness[i] = 0xFFFFFFFF;
+	}
+	for(size_t i = simdRects.count; i < simdRects.simdCount; i++)
+	{
+		simdRects.validness[i] = 0;
+	}
+
+	for(size_t i = 0; i < passRectIndices.size(); i++)
+	{
+		size_t rectIdx = passRectIndices[i];
+		simdRects.x1[i] = rects[rectIdx].x1;
+		simdRects.x2[i] = rects[rectIdx].x2;
+		simdRects.y1[i] = rects[rectIdx].y1;
+		simdRects.y2[i] = rects[rectIdx].y2;
+	}
+
+	nmsSimd1(simdRects, nmsThreshold);
+
+	std::vector<size_t> indices;
+	for(size_t i = 0; i < simdRects.count; i++)
+	{
+		if(simdRects.validness[i] != 0)
+		{
+			indices.push_back(passRectIndices[i]);
+		}
+	}
+
+	delete[] buffer;
+
+	return indices;
+}
--- test/test.cpp
+++ test/test.cpp
@@ -3,64 +3,65 @@
 #include <gtest/gtest.h>
 
 #include <opencv2/dnn/dnn.hpp>
-#include <absl/random/random.h>
-
+#include <random>
 #include <vector>
+
+#include <pcg_random.hpp>
 
 constexpr size_t g_rectangleSize = 1000;
 constexpr float g_scoreThreshold = 0.6;
 constexpr float g_nmsThreshold = 0.4;
 
-TEST(NMS, ValidnessTest)
+TEST(NMS, SyntheticData)
 {
-	absl::BitGen bitgen;
-
+	using namespace NMS_SIMD;
 	std::vector<int> rectanglesRaw(g_rectangleSize * 4);
 	std::vector<float> scores(g_rectangleSize);
 
+	pcg64 rng(42u, 54u);
+	std::uniform_int_distribution<int> pointDist(0, 1200);
+	std::uniform_int_distribution<int> sideDist(20, 60);
+	std::uniform_real_distribution<float> scoreDist(0, 1);
+
 	for(size_t i = 0; i < g_rectangleSize; i++)
 	{
-		int x1 = absl::Uniform<int>(bitgen, 0, 1200);
-		int y1 = absl::Uniform<int>(bitgen, 0, 1200);
-		int x2 = x1 + absl::Uniform<int>(bitgen, 20, 60);
-		int y2 = y1 + absl::Uniform<int>(bitgen, 20, 60);
+		int x1 = pointDist(rng);
+		int y1 = pointDist(rng);
+		int x2 = x1 + sideDist(rng);
+		int y2 = y1 + sideDist(rng);
 
 		rectanglesRaw[i * 4] = x1;
 		rectanglesRaw[i * 4 + 1] = y1;
 		rectanglesRaw[i * 4 + 2] = x2;
 		rectanglesRaw[i * 4 + 3] = y2;
-
-		scores[i] = absl::Uniform<float>(bitgen, 0, 1);
 	}
-#if 0
+
+	for(size_t i = 0; i < g_rectangleSize; i++)
+	{
+		scores[i] = scoreDist(rng);
+	}
+
 	std::vector<cv::Rect> cvRectangles(g_rectangleSize);
 	for(size_t i = 0; i < g_rectangleSize; i++)
 	{
 		cvRectangles[i] = cv::Rect(
 			rectanglesRaw[i * 4],
 			rectanglesRaw[i * 4 + 1],
-			rectanglesRaw[i * 4 + 2],
-			rectanglesRaw[i * 4 + 3]
+			rectanglesRaw[i * 4 + 2] - rectanglesRaw[i * 4],
+			rectanglesRaw[i * 4 + 3] - rectanglesRaw[i * 4 + 1]
 		);
 	}
 
 	cv::dnn::MatShape cvIndices;
-	for(size_t i = 0; i < 100000; i++)
-	{
-		cv::dnn::NMSBoxes(cvRectangles, scores, 0, g_nmsThreshold, cvIndices);
-		cvIndices.clear();
-	}
-#endif
+	cv::dnn::NMSBoxes(cvRectangles, scores, g_scoreThreshold, g_nmsThreshold, cvIndices);
 
-#if 2
 	std::vector<Rect> simdRectangles(g_rectangleSize);
 	std::copy(rectanglesRaw.data(), rectanglesRaw.data() + rectanglesRaw.size(), (int*)simdRectangles.data());
+	auto result = runNmsSimd1(simdRectangles, scores, g_scoreThreshold, g_nmsThreshold);
 
-	size_t a = 1;
-	for(size_t i = 0; i < 1; i++)
+	ASSERT_EQ(result.size(), cvIndices.size());
+	for(size_t i = 0; i < result.size(); i++)
 	{
-		nmsSimd1(simdRectangles, g_nmsThreshold);
+		EXPECT_EQ(result[i], cvIndices[i]) << "idx " << i;
 	}
-	std::cout << a << std::endl;
-#endif
 }
